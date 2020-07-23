b9684ffcd83ba0f40b829e806a536a678d6dd281
memmojtaba <mem.mojtaba@gmail.com>
2020-07-23 12:53:51 +0430

    Console logging and account status added.

--- login.py
+++ login.py
@@ -1,10 +1,17 @@
+#!/usr/bin/python3
+
 # Author: Mojtaba Miraghajanian
 # Email: mem.mojtaba@gmail.com
 
 import hashlib, requests, json, sched, time, sys, logging
 
 logging.basicConfig(level=logging.INFO, filename='app.log', filemode='a', format='%(asctime)s :: %(levelname)s: %(name)s --> %(message)s')
-
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+formatter = logging.Formatter('%(name)-8s: %(levelname)-8s: %(message)s')
+console.setFormatter(formatter)
+loginLogger = logging.getLogger('Login')
+loginLogger.addHandler(console)
 s = sched.scheduler(time.time, time.sleep)
 if(len(sys.argv) > 1 and sys.argv[1]):
   interval = float(sys.argv[1])
@@ -21,28 +28,42 @@ def doLogin(username, password):
   }
   check = checkLogin()
   if(check):
-    logging.info(check + ' => Login successful.')
+    loginLogger.info(check + ' => Login successful.\n' + remaining(check))
     return True
 
   else:
-    requests.post('https://hotspot.um.ac.ir/login', data=authData)
+    requests.post('https://hotspot.um.ac.ir/login', data = authData)
     check = checkLogin()
     if(check):
-      logging.info(check + ' => Login successful.')
+      loginLogger.info(check + ' => Login successful.\n' + remaining(check))
       return True
     else:
-      logging.warning(username + ' => Login failed.')
+      loginLogger.warning(username + ' => Login failed.')
       return False
 
 def checkLogin():
   # utf-8 byteCode of نام کاربری:
   usernameinBin = b'\xc3\x99\xc2\x86\xc3\x98\xc2\xa7\xc3\x99\xc2\x85 \xc3\x9a\xc2\xa9\xc3\x98\xc2\xa7\xc3\x98\xc2\xb1\xc3\x98\xc2\xa8\xc3\x98\xc2\xb1\xc3\x9b\xc2\x8c:'
   checkResponse = requests.get('http://hotspot.um.ac.ir/status')
+  # print(checkResponse.text.encode('utf8'))
   if (checkResponse.url.split('/')[-1] == 'login'):
     return 0
   else:
     ID = str(checkResponse.text.encode('utf8').split(usernameinBin)[1].split(b'\x3e')[1].split(b'\x3c')[0])
     return ID[2:len(ID)-1]
+
+def remaining(id):
+    usageData = {"username" : id}
+    usageResponse = requests.post('https://hs.um.ac.ir/usage.php', data = usageData)
+    res = json.loads(usageResponse.text)
+    monthly = (float(res['month']) * 1024)
+    consumed = (monthly - float(res['usage']))
+    remaining = (round((consumed / 1024), 1))
+    # return ("{:<24}Total: " + res['month'] + "\n{:<24}Used: " + 
+    #   str(round((float(res['usage']) / 1024), 1)) + "\n{:<24}Remainig: " + str(remaining)).format(' ', ' ', ' ')
+    return ("==========================\n" + "‖{:<4}Total:{:<5}" + res['month'] + "GB\t ‖" + 
+      "\n‖{:<4}Used:{:<6}" + str(round((float(res['usage']) / 1024), 1)) + "GB\t ‖" + 
+      "\n‖{:<4}Remainig:{:<2}" + str(remaining) + "GB\t ‖" + "\n==========================").format(' ',' ',' ',' ',' ',' ')
 
 
 def login():
--- logout.py
+++ logout.py
@@ -1,13 +1,21 @@
+#!/usr/bin/python3
+
 # Author: Mojtaba Miraghajanian
 # Email: mem.mojtaba@gmail.com
 
 import requests, time, logging
 
 logging.basicConfig(level=logging.INFO, filename='app.log', filemode='w', format='%(asctime)s :: %(levelname)s: %(name)s --> %(message)s')
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+formatter = logging.Formatter('%(name)-8s: %(levelname)-8s: %(message)s')
+console.setFormatter(formatter)
+logout = logging.getLogger('Logout')
+logout.addHandler(console)
 
 response = requests.get('http://hotspot.um.ac.ir/logout')
 checkResponse = requests.get('http://hotspot.um.ac.ir/status')
 if (checkResponse.url.split('/')[-1] == 'login'):
-    logging.info('Logout successful.')
+    logout.info('Logout successful.')
 else:
-    logging.warning('Logout failed.')
+    logout.warning('Logout failed.')
